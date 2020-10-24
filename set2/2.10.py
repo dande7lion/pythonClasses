c1a948e7cd470aba00fc84d43bc57a0a5f857815
dande7lion <halbiniak.natalia@gmail.com>
2020-10-24 11:11:20 +0200

    Added 2.10

---
+++ set2/2.10.py
@@ -0,0 +1,6 @@
+line = """Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+In eget dui nulla. Mauris faucibus posuere ante ut gravida.
+Mauris faucibus volutpat dictum."""
+
+words = line.split()
+print("Number of words: " + str(len(words)))
