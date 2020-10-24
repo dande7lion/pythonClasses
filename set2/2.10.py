7f84badc43345195da3dc434c9113a6222e61708
dande7lion <halbiniak.natalia@gmail.com>
2020-10-24 11:35:04 +0200

    Added tests to 2.10

--- set2/2.10.py
+++ set2/2.10.py
@@ -1,6 +1,23 @@
-line = """Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-In eget dui nulla. Mauris faucibus posuere ante ut gravida.
-Mauris faucibus volutpat dictum."""
+def number_of_words(line):
+    words = line.split()
+    return(len(words))
 
-words = line.split()
-print("Number of words: " + str(len(words)))
+if __name__ == "__main__":
+    line = """Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    In eget dui nulla. Mauris faucibus posuere ante ut gravida.
+    Mauris faucibus volutpat dictum."""
+
+    assert number_of_words(line) == 22
+
+    line = """Nulla volutpat hendrerit sapien, at tempus ipsum efficitur non. 
+    Duis blandit venenatis urna sit amet pulvinar. Morbi accumsan convallis sapien at
+     rutrum. Nam ullamcorper semper dignissim. Nunc efficitur at risus in vestibulum.
+      Sed elit urna, consequat dignissim ligula eu, posuere laoreet ante."""
+
+    assert number_of_words(line) == 42
+
+    line = """Lorem ipsum dolor sit amet, 
+    consectetur                 adipiscing elit."""
+
+    assert number_of_words(line) == 8
+    
