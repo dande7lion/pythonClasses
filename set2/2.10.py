d8c19a2d6e4776fe647ecdfd7f2434d9e0f19b46
dande7lion <halbiniak.natalia@gmail.com>
2020-10-24 12:18:54 +0200

    Tests using unittest

--- set2/2.10.py
+++ set2/2.10.py
@@ -1,23 +1,28 @@
+import unittest
+
 def number_of_words(line):
+    if not isinstance(line, str):
+        raise Exception ("Invalid parameter!")
     words = line.split()
     return(len(words))
 
+class TestNumberOfWords(unittest.TestCase):
+    def test_correct_line(self):
+        line = """Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        In eget dui nulla. Mauris faucibus posuere ante ut gravida.
+        Mauris faucibus volutpat dictum."""
+        self.assertEqual(number_of_words(line), 22)
+
+    def test_correct_line_with_tab(self):
+        line = """Lorem ipsum dolor sit amet, 
+        consectetur                 adipiscing elit."""
+        self.assertEqual(number_of_words(line), 8)
+
+    def test_incorrect_line(self):
+        line = 5
+        with self.assertRaises(Exception):
+            number_of_words(line)
+
+
 if __name__ == "__main__":
-    line = """Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    In eget dui nulla. Mauris faucibus posuere ante ut gravida.
-    Mauris faucibus volutpat dictum."""
-
-    assert number_of_words(line) == 22
-
-    line = """Nulla volutpat hendrerit sapien, at tempus ipsum efficitur non. 
-    Duis blandit venenatis urna sit amet pulvinar. Morbi accumsan convallis sapien at
-     rutrum. Nam ullamcorper semper dignissim. Nunc efficitur at risus in vestibulum.
-      Sed elit urna, consequat dignissim ligula eu, posuere laoreet ante."""
-
-    assert number_of_words(line) == 42
-
-    line = """Lorem ipsum dolor sit amet, 
-    consectetur                 adipiscing elit."""
-
-    assert number_of_words(line) == 8
-    
+    unittest.main()
