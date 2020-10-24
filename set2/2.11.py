3ecf0aa0a15b2f24c1f2310fb9528824531f7f57
dande7lion <halbiniak.natalia@gmail.com>
2020-10-24 12:26:04 +0200

    2.11 - added unit tests

--- set2/2.11.py
+++ set2/2.11.py
@@ -1,4 +1,17 @@
-word = "word"
-word = "_".join(word)
+import unittest
 
-print (word)
+def underscore_between_letters(word):
+    if not isinstance(word, str):
+        raise Exception ("Invalid parameter")
+    return "_".join(word)
+
+class TestUnderscoreBetweenLetters(unittest.TestCase):
+    def test_correct_word(self):
+        self.assertEqual(underscore_between_letters("word"), "w_o_r_d")
+
+    def test_incorrect_input(self):
+        with self.assertRaises(Exception):
+            underscore_between_letters(5)
+
+if __name__ == "__main__":
+    unittest.main()
