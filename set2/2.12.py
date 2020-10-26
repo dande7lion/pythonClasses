e1dd5e21e4203114f13555373ae1e7901ad8eb69
dande7lion <halbiniak.natalia@gmail.com>
2020-10-26 21:07:18 +0100

    Fixed 2.12

--- set2/2.12.py
+++ set2/2.12.py
@@ -3,22 +3,20 @@ import unittest
 def first_and_last_letters_in_line(line):
     if not isinstance(line, str):
         raise TypeError ("Invalid parameter!")
-    line = line.split("\n")
+    line = line.split()
     word_from_first_letters = ""
     word_from_last_letters = ""
-    for one_line in line:
-        one_line = one_line.strip()
-        word_from_first_letters += one_line[0]
-        word_from_last_letters += one_line[len(one_line)-1]
+    for word in line:
+        word = word.strip()
+        word_from_first_letters += word[0]
+        word_from_last_letters += word[len(word)-1]
 
     return word_from_first_letters, word_from_last_letters
 
 class WordsFromFirstAndLastLetters(unittest.TestCase):
     def test_correct_data(self):
-        line = """Lorem ipsum dolor sit amet, consectetur adipiscing elit
-        In eget dui nulla. Mauris faucibus posuere ante ut gravida
-        Mauris faucibus volutpat dictum"""
-        self.assertEqual(first_and_last_letters_in_line(line), ("LIM", "tam"))
+        line = "Lorem ipsum dolor sit amet consectetur adipiscing elit"
+        self.assertEqual(first_and_last_letters_in_line(line), ("Lidsacae", "mmrttrgt"))
 
     def test_incorrect_data(self):
         with self.assertRaises(TypeError):
