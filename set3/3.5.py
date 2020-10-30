968873e85b09d2b2893925249e2c001e457150e7
dande7lion <halbiniak.natalia@gmail.com>
2020-10-30 14:12:55 +0100

    3.5 - ruler_top and ruler_bottom to one string

--- set3/3.5.py
+++ set3/3.5.py
@@ -1,4 +1,4 @@
-def prepare_ruler(size) -> (str, str):
+def prepare_ruler(size) -> (str):
     if size == 0:
         return "|", "0"
     ruler_top = "|...."
@@ -7,14 +7,13 @@ def prepare_ruler(size) -> (str, str):
         ruler_bottom += "{:>5}".format(str(i+1))
         if i < size-2:
              ruler_top += "|...."
-    return ruler_top + "|", ruler_bottom
+    return ruler_top + "|\n" + ruler_bottom
 
 if __name__ == "__main__":
     length = input("Give length of the ruler: ")
     try:
         length = int(length)
-        ruler_top, ruler_bottom = prepare_ruler(length)
-        print(ruler_top)
-        print(ruler_bottom)
+        ruler = prepare_ruler(length)
+        print(ruler)
     except:
         print(f"{length} is not a valid length! Try again.")
