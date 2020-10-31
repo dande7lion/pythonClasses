8f251ee4b50092d8fac3293ec500c16f346ada00
dande7lion <halbiniak.natalia@gmail.com>
2020-10-31 10:13:30 +0100

    3.8 - fixed and tests added

--- set3/3.8.py
+++ set3/3.8.py
@@ -1,10 +1,12 @@
+import unittest
+
 def find_intersection(sequence1, sequence2) -> list:
     sequence1.sort()
     sequence2.sort()
     result_sequence = []
     for element1 in sequence1:
         for element2 in sequence2:
-            if element1 > element2:
+            if element1 < element2:
                 break
             elif element1 == element2:
                 if element1 not in result_sequence:
@@ -18,7 +20,14 @@ def find_sum(sequence1, sequence2) -> list:
             sequence1.append(element2)
     return sequence1
 
+class TestIntersectionAndSum(unittest.TestCase):
+    def test_intersection(self):
+        self.assertEqual(find_intersection(["a","b","c"],["b", "b", "d"]), ["b"])
+        self.assertEqual(find_intersection(["ab", "cd","ef"],["ef", "cd", "gh"]), ["cd", "ef"])
+
+    def test_sum(self):
+        self.assertEqual(find_sum(["a","b","c"],["b", "b", "d"]), ["a", "b", "c", "d"])
+        self.assertEqual(find_sum(["ab", "cd","ef"],["ef", "cd", "gh"]), ["ab", "cd", "ef", "gh"])
 
 if __name__ == "__main__":
-    print(find_intersection(["a","b","c"],["b", "b", "d"]))
-    print(find_sum(["a","b","c"],["b", "b", "d"]))
+    unittest.main()
