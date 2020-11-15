b2e8b526814362e3dd3033a561afd10fd4a9c8da
dande7lion <halbiniak.natalia@gmail.com>
2020-11-15 15:19:20 +0100

    Added sub_frac function + refactoring

--- set5/fracs.py
+++ set5/fracs.py
@@ -1,6 +1,9 @@
 import fractions
 
-def add_frac(frac1, frac2):         # frac1 + frac2    
+def is_fraction_correct(frac):
+    return True if frac[1] is not 0 else False
+
+def convert_fractions_to_a_common_denominator(frac1, frac2):
     least_common_multiplier = frac1[1]*frac2[1] // fractions.gcd(frac1[1], frac2[1])
     multiplier_1 = least_common_multiplier//frac1[1]
     multiplier_2 = least_common_multiplier//frac2[1]
@@ -8,9 +11,26 @@ def add_frac(frac1, frac2):         # frac1 + frac2
     frac2[0] = frac2[0]*multiplier_2
     frac1[1] = least_common_multiplier
     frac2[1] = least_common_multiplier
-    return [frac1[0] + frac2[0], frac1[1]]
+    return [frac1, frac2]
 
-def sub_frac(frac1, frac2): pass        # frac1 - frac2
+# e.g. [-11, -15] -> [11, 15] (both are correct, the second form is used by default)
+def convert_negative_to_positive(frac):
+    if frac[0] < 0 and frac[1] < 0:
+        frac[0] = abs(frac[0])
+        frac[1] = abs(frac[1])
+    return frac
+
+# frac1 + frac2
+def add_frac(frac1, frac2):
+    if not is_fraction_correct(frac1) or not is_fraction_correct(frac2):
+        raise TypeError("Denominator can not be zero!")
+    frac1, frac2 = convert_fractions_to_a_common_denominator(frac1, frac2)  
+    return convert_negative_to_positive([frac1[0] + frac2[0],frac1[1]])
+
+# frac1 - frac2
+def sub_frac(frac1, frac2):
+    frac1, frac2 = convert_fractions_to_a_common_denominator(frac1, frac2)  
+    return convert_negative_to_positive([frac1[0] - frac2[0], frac1[1]])
 
 def mul_frac(frac1, frac2): pass        # frac1 * frac2
 
@@ -40,9 +60,16 @@ class TestFractions(unittest.TestCase):
     def test_add_frac(self):
         self.assertEqual(add_frac([3, 5], [4, 3]), [29, 15])
         self.assertEqual(add_frac([17, 83], [54, 18]), [4788, 1494])
-        self.assertEqual(add_frac([3, -5], [4, -3]), [-29, 15])
+        self.assertEqual(add_frac([3, -5], [4, -3]), [29, -15])
+        with self.assertRaises(TypeError):
+            add_frac([5, 0], [4, 8])
 
-    def test_sub_frac(self): pass
+    def test_sub_frac(self):
+        self.assertEqual(sub_frac([3, 5], [4, 3]), [-11, 15])
+        self.assertEqual(sub_frac([17, 83], [54, 18]), [-4176, 1494])
+        self.assertEqual(sub_frac([3, -5], [4, -3]), [11, 15])
+        with self.assertRaises(TypeError):
+            add_frac([5, 0], [4, 8])
 
     def test_mul_frac(self): pass
 
