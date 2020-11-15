f73e0f4a474c9f805357d1630a0e491d644ca908
dande7lion <halbiniak.natalia@gmail.com>
2020-11-15 16:46:46 +0100

    Set 5 finished

--- set5/fracs.py
+++ set5/fracs.py
@@ -24,34 +24,36 @@ def convert_negative_to_positive(frac):
 # frac1 + frac2
 def add_frac(frac1, frac2):
     if not is_fraction_correct(frac1) or not is_fraction_correct(frac2):
-        raise TypeError("Denominator can not be zero or numbers have to be intigers!")
+        raise ("Denominator can not be zero or numbers have to be integers!")
     frac1, frac2 = convert_fractions_to_a_common_denominator(frac1, frac2)  
     return convert_negative_to_positive([frac1[0] + frac2[0],frac1[1]])
 
 # frac1 - frac2
 def sub_frac(frac1, frac2):
     if not is_fraction_correct(frac1) or not is_fraction_correct(frac2):
-        raise TypeError("Denominator can not be zero or numbers have to be intigers!")
+        raise Exception("Denominator can not be zero or numbers have to be integers!")
     frac1, frac2 = convert_fractions_to_a_common_denominator(frac1, frac2)  
     return convert_negative_to_positive([frac1[0] - frac2[0], frac1[1]])
 
 # frac1 * frac2
 def mul_frac(frac1, frac2):
     if not is_fraction_correct(frac1) or not is_fraction_correct(frac2):
-        raise TypeError("Denominator can not be zero or numbers have to be intigers!")
+        raise Exception("Denominator can not be zero or numbers have to be integers!")
     frac1[0] = frac1[0]*frac2[0]
     frac1[1] = frac1[1]*frac2[1]
     return convert_negative_to_positive(frac1)
 
 # frac1 / frac2
 def div_frac(frac1, frac2):
+    if not is_fraction_correct(frac2):
+        raise Exception("Denominator can not be zero or numbers have to be integers!")
     frac2[0], frac2[1] = frac2[1], frac2[0]
     return mul_frac(frac1, frac2)
 
 # bool, czy dodatni
 def is_positive(frac):
     if not is_fraction_correct(frac):
-        raise TypeError("Denominator can not be zero or numbers have to be intigers!")
+        raise Exception("Denominator can not be zero or numbers have to be integers!")
     frac = convert_negative_to_positive(frac)
     if frac[0] < 0 and frac[1] < 0 or frac[0] > 0 and frac[1] > 0: return True
     return False
@@ -59,25 +61,23 @@ def is_positive(frac):
 # bool, typu [0, x]
 def is_zero(frac):
     if not is_fraction_correct(frac):
-        raise TypeError("Denominator can not be zero or numbers have to be intigers!")
+        raise Exception("Denominator can not be zero or numbers have to be integers!")
     return True if frac[0] == 0 else False
 
 # -1 | 0 | +1
 def cmp_frac(frac1, frac2):
     if not is_fraction_correct(frac1) or not is_fraction_correct(frac2):
-        raise TypeError("Denominator can not be zero or numbers have to be intigers!")
+        raise Exception("Denominator can not be zero or numbers have to be integers!")
     frac1, frac2 = convert_fractions_to_a_common_denominator(frac1, frac2)
     if frac1[0] > frac2[0]: return 1
     if frac1[0] < frac2[0]: return -1
     else: return 0
 
-def frac2float(frac): pass              # konwersja do float
-
-f1 = [-1, 2]                  # -1/2
-f2 = [0, 1]                   # zero
-f3 = [3, 1]                   # 3
-f4 = [6, 2]                   # 3 (niejednoznaczność)
-f5 = [0, 2]                   # zero (niejednoznaczność)
+# konwersja do float
+def frac2float(frac):
+    if not is_fraction_correct(frac):
+        raise Exception("Denominator can not be zero or numbers have to be integers!")
+    return float(frac[0]) / float(frac[1])
 
 import unittest
 
@@ -90,21 +90,24 @@ class TestFractions(unittest.TestCase):
         self.assertEqual(add_frac([3, 5], [4, 3]), [29, 15])
         self.assertEqual(add_frac([17, 83], [54, 18]), [4788, 1494])
         self.assertEqual(add_frac([3, -5], [4, -3]), [29, -15])
-        with self.assertRaises(TypeError):
+        self.assertEqual(add_frac(self.zero, [4, -3]), [-4, 3])
+        with self.assertRaises(Exception):
             add_frac([5, 0], [4, 8])
 
     def test_sub_frac(self):
         self.assertEqual(sub_frac([3, 5], [4, 3]), [-11, 15])
         self.assertEqual(sub_frac([17, 83], [54, 18]), [-4176, 1494])
         self.assertEqual(sub_frac([3, -5], [4, -3]), [11, 15])
-        with self.assertRaises(TypeError):
+        self.assertEqual(sub_frac(self.zero, [4, -3]), [4, 3])
+        with self.assertRaises(Exception):
             sub_frac([5, 0], [4, 8])
 
     def test_mul_frac(self):
         self.assertEqual(mul_frac([3, 5], [4, 3]), [12, 15])
         self.assertEqual(mul_frac([7, 18], [14, -3]), [98, -54])
         self.assertEqual(mul_frac([-3, -5], [-4, -3]), [12, 15])
-        with self.assertRaises(TypeError):
+        self.assertEqual(mul_frac(self.zero, [-4, -3]), self.zero)
+        with self.assertRaises(Exception):
             mul_frac([7, 0], [-4, 15])
 
 
@@ -112,31 +115,36 @@ class TestFractions(unittest.TestCase):
         self.assertEqual(div_frac([3, 5], [4, 3]), [9, 20])
         self.assertEqual(div_frac([-3, 5], [4, -3]), [9, 20])
         self.assertEqual(div_frac([9, 15], [14, -3]), [-27, 210])
-        with self.assertRaises(TypeError):
+        with self.assertRaises(Exception):
             div_frac([7, 4], [0, 15])
+        with self.assertRaises(Exception):
+            div_frac([4, 5], self.zero)
 
     def test_is_positive(self):
         self.assertTrue(is_positive([5, 4]))
         self.assertTrue(is_positive([-14, -8]))
         self.assertFalse(is_positive([5, -8]))
-        with self.assertRaises(TypeError):
+        with self.assertRaises(Exception):
             is_positive([7.8, 4.4])
 
     def test_is_zero(self):
         self.assertTrue(is_zero([0, 6]))
         self.assertFalse(is_zero([4, 5]))
         self.assertFalse(is_zero([-15, 4]))
-        with self.assertRaises(TypeError):
+        with self.assertRaises(Exception):
             is_zero([0, 0])
 
     def test_cmp_frac(self):
         self.assertEqual(cmp_frac([3, 5], [4, 3]), -1)
         self.assertEqual(cmp_frac([3, -5], [4, 3]), 1)
         self.assertEqual(cmp_frac([3, 5], [3, 5]), 0)
-        with self.assertRaises(TypeError):
+        with self.assertRaises(Exception):
             cmp_frac([12, 0], [7.8, 8])
 
-    def test_frac2float(self): pass
+    def test_frac2float(self):
+        self.assertEqual(frac2float([2, 5]), 0.4)
+        self.assertEqual(round(frac2float([19, 47]), 2), 0.40)
+        self.assertEqual(round(frac2float([8, 63]), 5), 0.12698)
 
     def tearDown(self): pass
 
